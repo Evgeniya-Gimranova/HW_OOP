7fa6ba04d92f9105dc4225459cd0e962ba067a0f
evgeniya <evgeniya2689@yandex.ru>
2023-10-26 12:52:28 +0500

    Add files

---
+++ HW4/Main.java
@@ -0,0 +1,21 @@
+package HW4;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassOfTeachers<Teacher> teachers = new ClassOfTeachers<>("teacherss");
+        teachers.add(new ChemistryTeacher("Peter", "Ivanych"));
+        teachers.add(new ShopTeacher("Agafiya", "Sergeevna"));
+
+        for (int index = 0; index < teachers.count(); index++) {
+            System.out.println(teachers.get(index));
+        }
+
+
+
+
+    }
+}
